9834f553ab783cecd200534de8d0809d4c70d37c
Max Ruenz <max.ruenz@web.de>
2017-12-02 17:30:36 +0100

    Adapt par cor to new d3 version

--- mainReloaded.js
+++ mainReloaded.js
@@ -266,6 +266,18 @@ $(function() {
         .range([height, 0]));
     }));
 
+    console.log(dimensions);
+    let valRanges = dimensions.map(function(d) {
+      return d3.extent(cars, function(p) {
+        return +p[d];
+      })
+    });
+    let valRangesObj = {};
+    for (let i = 0; i < dimensions.length; i++) {
+      valRangesObj[dimensions[i]] = valRanges[i];
+    }
+    console.log(valRangesObj);
+
     // Add grey background lines for context.
     background = svg.append("g")
       .attr("class", "background")
@@ -287,6 +299,7 @@ $(function() {
       .data(dimensions)
       .enter().append("g")
       .attr("class", "dimension")
+      .attr("class", "brush")
       .attr("transform", function(d) {
         return "translate(" + x(d) + ")";
       })
@@ -340,47 +353,61 @@ $(function() {
     g.append("g")
       .attr("class", "brush")
       .each(function(d) {
-        d3.select(this).call(y[d].brush = d3.brushY(y[d]).on("start", brushstart).on("brush", brush));
+        d3.select(this).call(y[d].brush = d3.brushY(y[d])
+                          .extent([[-8,0], [8,height]])
+                          .on("start", brushstart)
+                          .on("brush", brush));
       })
       .selectAll("rect")
       .attr("x", -8)
       .attr("width", 16);
+
+
+
+    function position(d) {
+      var v = dragging[d];
+      return v == null ? x(d) : v;
+    }
+
+    function transition(g) {
+      return g.transition().duration(500);
+    }
+
+    // Returns the path for a given data point.
+    function path(d) {
+      return line(dimensions.map(function(p) {
+        return [position(p), y[p](d[p])];
+      }));
+    }
+
+    function brushstart() {
+      d3.event.sourceEvent.stopPropagation();
+    }
+
+    // Handles a brush event, toggling the display of foreground lines.
+    function brush() {
+
+      var actives = [];
+      svg.selectAll(".brush")
+        .filter(function(d) {
+          return d3.brushSelection(this);
+        })
+        .each(function(d) {
+          actives.push({
+            dimension: d,
+            extent: d3.brushSelection(this)
+          });
+        });
+
+      foreground.style("display", function(d) {
+        return actives.every(function(p) {
+          let scaled = d3.scaleLinear().domain(valRangesObj[p.dimension]).range([height, 0])(d[p.dimension]);
+          return p.extent[0] <= scaled && scaled <= p.extent[1];
+        }) ? null : "none";
+      });
+    }
   });
 
-  function position(d) {
-    var v = dragging[d];
-    return v == null ? x(d) : v;
-  }
-
-  function transition(g) {
-    return g.transition().duration(500);
-  }
-
-  // Returns the path for a given data point.
-  function path(d) {
-    return line(dimensions.map(function(p) {
-      return [position(p), y[p](d[p])];
-    }));
-  }
-
-  function brushstart() {
-    d3.event.sourceEvent.stopPropagation();
-  }
-
-  // Handles a brush event, toggling the display of foreground lines.
-  function brush() {
-    var actives = dimensions.filter(function(p) {
-        return !y[p].brush.empty();
-      }),
-      extents = actives.map(function(p) {
-        return y[p].brush.extent();
-      });
-    foreground.style("display", function(d) {
-      return actives.every(function(p, i) {
-        return extents[i][0] <= d[p] && d[p] <= extents[i][1];
-      }) ? null : "none";
-    });
-  }
 
   let ctx4 = document.getElementById("chart-evol");
   let myChart4 = new Chart(ctx4, {
