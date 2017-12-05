1d5c526cd50d71996ab488746919ae9c0887ed72
Max Ruenz <max.ruenz@web.de>
2017-12-05 16:06:59 +0100

    Load charts asynchronously

--- mainReloaded.js
+++ mainReloaded.js
@@ -8,137 +8,85 @@ $(function() {
     wrap: false
   });
 
-  let ctx = document.getElementById("chart-descre");
-  let myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: [1.00000000e+00, 3.60000000e+02, 7.19000000e+02,
-        1.07800000e+03, 1.43700000e+03, 1.79600000e+03,
-        2.15500000e+03, 2.51400000e+03, 2.87300000e+03,
-        3.23200000e+03, 3.59100000e+03
-      ],
-      datasets: [{
-        label: '# of Users',
-        data: [4.17112000e+05, 7.70000000e+01, 1.20000000e+01,
-          8.00000000e+00, 5.00000000e+00, 1.00000000e+00,
-          1.00000000e+00, 1.00000000e+00, 0.00000000e+00,
-          2.00000000e+00
-        ],
-        backgroundColor: [
-          '#4292c6',
-          '#4292c6',
-          '#4292c6',
-          '#4292c6',
-          '#4292c6',
-          '#4292c6',
-          '#4292c6',
-          '#4292c6',
-          '#4292c6',
-          '#4292c6'
-        ]
+  let optionsHist = {
+    scales: {
+      yAxes: [{
+        type: 'logarithmic',
+        ticks: {
+          beginAtZero: true,
+          padding: 20,
+          fontColor: '#ffffff'
+        },
+        gridLines: {
+          color: '#d3d3d3'
+        },
+        scaleLabel: {}
+      }],
+      xAxes: [{
+        gridLines: {
+          color: '#ffffff',
+          display: false
+        },
+        ticks: {
+          fontColor: '#ffffff'
+        }
       }]
     },
-    options: {
-      scales: {
-        yAxes: [{
-          type: 'logarithmic',
-          ticks: {
-            beginAtZero: true,
-            padding: 20,
-            fontColor: '#ffffff'
-          },
-          gridLines: {
-            color: '#d3d3d3'
-          },
-          scaleLabel: {}
-        }],
-        xAxes: [{
-          gridLines: {
-            color: '#ffffff',
-            display: false
-          },
-          ticks: {
-            fontColor: '#ffffff'
-          }
-        }]
-      },
-      legend: {
-        position: 'bottom'
-      }
+    legend: {
+      position: 'bottom'
     }
+  };
+
+  let ctx = document.getElementById("chart-descre");
+  let myChart;
+
+  d3.json("data/reputation_hist.json", function(json) {
+    let data = json;
+    data.datasets[0].backgroundColor = ["#4292c6", "#4292c6", "#4292c6", "#4292c6", "#4292c6", "#4292c6", "#4292c6", "#4292c6", "#4292c6", "#4292c6"];
+    myChart = new Chart(ctx, {
+      type: 'bar',
+      data: data,
+      options: optionsHist
+    });
   });
 
   let ctx2 = document.getElementById("chart-class");
-  let myChart2 = new Chart(ctx2, {
-    type: 'bar',
-    data: {
-      labels: [1.00000000e+00, 3.60000000e+02, 7.19000000e+02,
-        1.07800000e+03, 1.43700000e+03, 1.79600000e+03,
-        2.15500000e+03, 2.51400000e+03, 2.87300000e+03,
-        3.23200000e+03, 3.59100000e+03
-      ],
-      datasets: [{
-        label: '# of Users',
-        data: [4.17112000e+05, 7.70000000e+01, 1.20000000e+01,
-          8.00000000e+00, 5.00000000e+00, 1.00000000e+00,
-          1.00000000e+00, 1.00000000e+00, 0.00000000e+00,
-          2.00000000e+00
-        ],
-        backgroundColor: ['#eff3ff', '#bdd7e7', '#bdd7e7', '#6baed6', '#6baed6', '#3182bd', '#3182bd', '#3182bd', '#3182bd', '#08519c']
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          type: 'logarithmic',
-          ticks: {
-            beginAtZero: true,
-            padding: 20,
-            fontColor: '#ffffff'
-          },
-          gridLines: {
-            color: '#d3d3d3'
-          },
-          scaleLabel: {}
-        }],
-        xAxes: [{
-          gridLines: {
-            color: '#ffffff',
-            display: false
-          },
-          ticks: {
-            fontColor: '#ffffff'
-          }
-        }]
-      },
-      legend: {
-        position: 'bottom'
-      }
-    }
+  let myChart2
+
+  d3.json("data/classes.json", function(json) {
+    let data = json;
+    data.datasets[0].backgroundColor = ['#eff3ff', '#bdd7e7', '#bdd7e7', '#6baed6', '#6baed6', '#3182bd', '#3182bd', '#3182bd', '#3182bd', '#08519c'];
+
+    myChart2 = new Chart(ctx2, {
+      type: 'bar',
+      data: data,
+      options: optionsHist
+    });
+
   });
 
+
   let ctx3 = document.getElementById("chart-contrib");
-  let myChart3 = new Chart(ctx3, {
-    type: 'horizontalBar',
-    data: {
-      labels: ["One time users",
-        "Occasinal users",
-        "Common users",
-        "Frequent users",
-        "Super users"
-      ].reverse(),
-      datasets: [{
-        label: "Votes",
-        data: [1, 2, 4, 8, 16].reverse(),
-        backgroundColor: ['#eff3ff', '#bdd7e7', '#6baed6', '#3182bd', '#08519c'].reverse()
-      }]
-    },
-    options: {
-      legend: {
-        position: 'bottom'
+  let myChart3;
+  let user_classes_feature = "Votes";
+  d3.json("data/users_classes.json", function(json) {
+    let data = json;
+    data[user_classes_feature].datasets[0].backgroundColor = ['#eff3ff', '#bdd7e7', '#6baed6', '#3182bd', '#08519c'];
+
+    // TODO fill table
+
+
+    myChart3 = new Chart(ctx3, {
+      type: 'horizontalBar',
+      data: data[user_classes_feature],
+      options: {
+        legend: {
+          position: 'bottom'
+        }
       }
-    }
+    });
   });
+
 
   // stream Chart
 
@@ -146,109 +94,97 @@ $(function() {
     "Questions One Time User": '#ffffff'
   };
 
-  let layers = [
-    {
-      label: "Questions One Time User",
-      data: [
-        [0, 1, 2],
-        [1, 1, 2],
-        [2, 1, 3]
-      ]
+  d3.json("data/QA_time.json", function(json) {
+    let layers = json;
+
+    var svg = d3.select("#div-stream").append("svg").attr('width', 1200).attr('height', 400),
+      width = 1200,
+      height = 400;
+
+    var x = d3.scaleLinear()
+      .domain([d3.min(layers, xMin), d3.max(layers, xMax)])
+      .range([0, width]);
+
+    var y = d3.scaleLinear()
+      .domain([d3.min(layers, stackMin), d3.max(layers, stackMax)])
+      .range([height, 0]);
+
+    var z = d3.interpolateCool;
+
+    var area = d3.area()
+      .x(function(d) {
+        return x(d[0]);
+      })
+      .y0(function(d) {
+        return y(d[1]);
+      })
+      .y1(function(d) {
+        return y(d[2]);
+      });
+
+    svg.selectAll("path")
+      .data(layers)
+      .enter().append("path")
+      .attr("d", function(d) {
+        return area(d.data)
+      })
+      .attr("fill", function(d) {
+        return colorMap[d.label];
+      })
+      .on("mouseover", function(d) {
+
+        let rect = document.getElementById('div-stream').getBoundingClientRect();
+        var xPosition = d3.event.pageX - rect.x;
+        var yPosition = d3.event.pageY;
+
+        d3.select("#tooltip")
+          .style("left", xPosition + "px")
+          .style("top", yPosition + "px")
+          .select("#value")
+          .text(d.label);
+
+        d3.select("#tooltip").classed("hidden", false);
+      })
+      .on("mouseout", function() {
+        d3.select("#tooltip").classed("hidden", true);
+      });
+
+    function xMax(layer) {
+      let data = layer.data;
+      return d3.max(data, function(d) {
+        return d[0];
+      });
     }
-  ]
 
-  console.log(layers);
+    function xMin(layer) {
+      let data = layer.data;
+      return d3.min(data, function(d) {
+        return d[0];
+      });
+    }
 
-  var svg = d3.select("#div-stream").append("svg").attr('width', 1200).attr('height', 400),
-    width = 1200,
-    height = 400;
+    function stackMax(layer) {
+      let data = layer.data;
+      return d3.max(data, function(d) {
+        return d[2];
+      });
+    }
 
-  var x = d3.scaleLinear()
-    .domain([d3.min(layers, xMin), d3.max(layers, xMax)])
-    .range([0, width]);
+    function stackMin(layer) {
+      let data = layer.data;
+      return d3.min(data, function(d) {
+        return d[1];
+      });
+    }
 
-  var y = d3.scaleLinear()
-    .domain([d3.min(layers, stackMin), d3.max(layers, stackMax)])
-    .range([height, 0]);
+  });
 
-  var z = d3.interpolateCool;
 
-  var area = d3.area()
-    .x(function(d) {
-      return x(d[0]);
-    })
-    .y0(function(d) {
-      return y(d[1]);
-    })
-    .y1(function(d) {
-      return y(d[2]);
-    });
-
-  svg.selectAll("path")
-    .data(layers)
-    .enter().append("path")
-    .attr("d", function (d) {return area(d.data)})
-    .attr("fill", function(d) {
-      return colorMap[d.label];
-    })
-    .on("mouseover", function(d) {
-
-          let rect = document.getElementById('div-stream').getBoundingClientRect();
-					var xPosition = d3.event.pageX - rect.x;
-					var yPosition = d3.event.pageY;
-
-					d3.select("#tooltip")
-						.style("left", xPosition + "px")
-						.style("top", yPosition + "px")
-						.select("#value")
-						.text(d.label);
-
-					d3.select("#tooltip").classed("hidden", false);
-			   })
-	   .on("mouseout", function() {
-			    d3.select("#tooltip").classed("hidden", true);
-	   });
-
-  function xMax(layer){
-    let data = layer.data;
-    return d3.max(data, function(d) {
-      return d[0];
-    });
-  }
-
-  function xMin(layer){
-    let data = layer.data;
-    return d3.min(data, function(d) {
-      return d[0];
-    });
-  }
-
-  function stackMax(layer) {
-    let data = layer.data;
-    return d3.max(data, function(d) {
-      return d[2];
-    });
-  }
-
-  function stackMin(layer) {
-    let data = layer.data;
-    return d3.min(data, function(d) {
-      return d[1];
-    });
-  }
 
   // parallel cordinates
 
-    //let widthPC = $("#div-cordinates").width(),
-    //heightPC = $("#div-cordinates").height();
-
-
-    let widthPC = 500 - 50,
+  let widthPC = 500 - 50,
     heightPC = 500 - 50;
-
-
-    console.log(widthPC);
-    console.log(heightPC);
 
   var x = d3.scalePoint().range([0, widthPC], 1),
     y = {},
@@ -265,11 +201,11 @@ $(function() {
     .attr("height", heightPC + 'px')
     .append("g");
 
-  d3.csv("cars.csv", function(error, cars) {
+  d3.csv("data/users.csv", function(error, cars) {
 
     // Extract the list of dimensions and create a scale for each.
     x.domain(dimensions = d3.keys(cars[0]).filter(function(d) {
-      return d != "name" && (y[d] = d3.scaleLinear()
+      return (y[d] = d3.scaleLinear()
         .domain(d3.extent(cars, function(p) {
           return +p[d];
         }))
@@ -363,9 +299,12 @@ $(function() {
       .attr("class", "brush")
       .each(function(d) {
         d3.select(this).call(y[d].brush = d3.brushY(y[d])
-                          .extent([[-8,0], [8,heightPC]])
-                          .on("start", brushstart)
-                          .on("brush", brush));
+          .extent([
+            [-8, 0],
+            [8, heightPC]
+          ])
+          .on("start", brushstart)
+          .on("brush", brush));
       })
       .selectAll("rect")
       .attr("x", -8)
@@ -417,111 +356,91 @@ $(function() {
     }
   });
 
-
-  let ctx4 = document.getElementById("chart-evol");
-  let myChart4 = new Chart(ctx4, {
-    type: 'bubble',
-    data: {
-      datasets: [{
-        data: [{
-            x: 1, // value of feature
-            y: 0, // Class of users
-            v: 10 // Number of users
-          },
-          {
-            x: 10, // value of feature
-            y: 1, // Class of users
-            v: 8 // Number of users
-          },
-          {
-            x: 120, // value of feature
-            y: 2, // Class of users
-            v: 4 // Number of users
-          },
-          {
-            x: 998, // value of feature
-            y: 3, // Class of users
-            v: 2 // Number of users
-          },
-          {
-            x: 1500, // value of feature
-            y: 4, // Class of users
-            v: 1 // Number of users
-          }
-        ]
+  let optionsBubble = {
+    aspectRatio: 1,
+    elements: {
+      point: {
+        radius: function(context) {
+          var value = context.dataset.data[context.dataIndex];
+          var size = context.chart.width;
+          var base = Math.abs(value.v) / 10;
+          return (size / 24) * base;
+        },
+        backgroundColor: function(context) {
+          var value = context.dataset.data[context.dataIndex];
+          return ['#eff3ff', '#bdd7e7', '#6baed6', '#3182bd', '#08519c'][value.y];
+        }
+      }
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          padding: 20,
+          fontColor: '#ffffff'
+        },
+        gridLines: {
+          color: '#d3d3d3'
+        },
+        scaleLabel: {}
+      }],
+      xAxes: [{
+        type: 'logarithmic',
+        gridLines: {
+          color: '#ffffff',
+          display: false
+        },
+        ticks: {
+          fontColor: '#ffffff'
+        }
       }]
     },
-    options: {
-      aspectRatio: 1,
-      elements: {
-        point: {
-          radius: function(context) {
-            var value = context.dataset.data[context.dataIndex];
-            var size = context.chart.width;
-            var base = Math.abs(value.v) / 10;
-            return (size / 24) * base;
-          },
-          backgroundColor: function(context) {
-            var value = context.dataset.data[context.dataIndex];
-            return ['#eff3ff', '#bdd7e7', '#6baed6', '#3182bd', '#08519c'][value.y];
-          }
-        }
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true,
-            padding: 20,
-            fontColor: '#ffffff'
-          },
-          gridLines: {
-            color: '#d3d3d3'
-          },
-          scaleLabel: {}
-        }],
-        xAxes: [{
-          type: 'logarithmic',
-          gridLines: {
-            color: '#ffffff',
-            display: false
-          },
-          ticks: {
-            fontColor: '#ffffff'
-          }
-        }]
-      },
-      legend: false
-    }
-  });
+    legend: false
+  };
 
-  $('#ex1').slider({
-    formatter: function(value) {
-      return 'Current value: ' + value;
-    }
+  let ctx4 = document.getElementById("chart-evol");
+  let myChart4,
+    year = "2008";
+  d3.json("data/users_classes_evol.json", function(json) {
+    let data = json[year]
+    myChart4 = new Chart(ctx4, {
+      type: 'bubble',
+      data: data,
+      options: optionsBubble
+    });
+
+    $('#ex1').slider({
+      formatter: function(value) {
+        return 'Current value: ' + value;
+      }
+    });
   });
 
 
   let ctx5 = document.getElementById("chart-cust");
-  let myChart5 = new Chart(ctx5, {
-    type: 'horizontalBar',
-    data: {
-      labels: ["One time users",
-        "Occasinal users",
-        "Common users",
-        "Frequent users",
-        "Super users"
-      ].reverse(),
-      datasets: [{
-        label: "",
-        data: [1, 2, 4, 8, 16].reverse(),
-        backgroundColor: ['#eff3ff', '#bdd7e7', '#6baed6', '#3182bd', '#08519c'].reverse()
-      }]
-    },
-    options: {
-      legend: {
-        position: 'bottom'
-      }
-    }
+  d3.csv("data/users.csv", function (csv) {
+      // TODO calcuate classes and create table
+      let myChart5 = new Chart(ctx5, {
+        type: 'horizontalBar',
+        data: {
+          labels: ["One time users",
+            "Occasinal users",
+            "Common users",
+            "Frequent users",
+            "Super users"
+          ].reverse(),
+          datasets: [{
+            label: "",
+            data: [1, 2, 4, 8, 16].reverse(),
+            backgroundColor: ['#eff3ff', '#bdd7e7', '#6baed6', '#3182bd', '#08519c'].reverse()
+          }]
+        },
+        options: {
+          legend: {
+            position: 'bottom'
+          }
+        }
+      });
   });
 
   $('#slider').slider({
