4f5e7f6cff294f6f8ac43e977660562181dfc2ec
Max Ruenz <max.ruenz@web.de>
2017-12-07 11:07:30 +0100

    Cleanup ParrallelCordinates

--- scripts/Step6.js
+++ scripts/Step6.js
@@ -1,36 +1,37 @@
-define(['d3', 'charts/StreamGraph'], function(d3, StreamGraph){
+define(['d3', 'charts/StreamGraph'], function(d3, StreamGraph) {
 
-    function initializePage(){
-      console.log("Hi");
-      const margins = {
+  function initializePage() {
+    console.log("Hi");
+    const margins = {
         top: 30,
         right: 10,
         bottom: 10,
         left: 50
       },
       bounds = d3.select('#div-stream').node().getBoundingClientRect(),
-        widthSC = bounds.width - margins.left - margins.right,
-        heightSC = bounds.height - margins.top - margins.bottom,
-        element = "#div-stream"
+      widthSC = bounds.width - margins.left - margins.right,
+      heightSC = bounds.height - margins.top - margins.bottom,
+      element = "#div-stream"
 
-      const colorMap = {
-        "Questions": '#ff0000',
-        "Answers": '#0000ff'
-      };
-
-      d3.json("data/QA_time.json", function(data) {
-        pC = new StreamGraph({
-          element: element,
-          height: heightSC,
-          width: widthSC,
-          margins: margins,
-          data: data,
-          colorMap, colorMap
-        });
-      });
-    }
-
-    return {
-      initializePage: initializePage
+    const colorMap = {
+      "Questions": '#ff0000',
+      "Answers": '#0000ff'
     };
+
+    d3.json("data/QA_time.json", function(data) {
+      sG = new StreamGraph({
+        element: element,
+        height: heightSC,
+        width: widthSC,
+        margins: margins,
+        data: data,
+        colorMap,
+        colorMap
+      });
+    });
+  }
+
+  return {
+    initializePage: initializePage
+  };
 });
--- scripts/charts/StreamGraph.js
+++ scripts/charts/StreamGraph.js
@@ -3,91 +3,126 @@ define(['d3'], function(d3) {
   class StreamGraph {
     constructor(config) {
 
-      let element = config.element,
-        height = config.height,
-        width = config.width,
-        margins = config.margins,
-        data = config.data,
-        colorMap = config.colorMap;
+      this.element = config.element;
+      this.height = config.height;
+      this.width = config.width;
+      this.margins = config.margins;
+      this.data = config.data;
+      this.colorMap = config.colorMap;
 
-      let svg = d3.select(element).append("svg").attr('width', width).attr('height', height);
+      this.svg = d3.select(this.element)
+        .append("svg")
+        .attr('width', this.width)
+        .attr('height', this.height)
+        .append("g");
 
-      let x = d3.scaleLinear()
-        .domain([d3.min(data, xMin), d3.max(data, xMax)])
-        .range([0, width]);
+      this.x = d3.scaleLinear()
+        .domain([d3.min(this.data, xMin), d3.max(this.data, xMax)])
+        .range([0, this.width]);
 
-      let y = d3.scaleLinear()
-        .domain([d3.min(data, stackMin), d3.max(data, stackMax)])
-        .range([height, 0]);
+      this.y = d3.scaleLinear()
+        .domain([d3.min(this.data, stackMin), d3.max(this.data, stackMax)])
+        .range([this.height, 0]);
 
-      let z = d3.interpolateCool;
+      this.z = d3.interpolateCool;
 
-      let area = d3.area()
+      this.area = d3.area()
         .x(function(d) {
-          return x(d[0]);
-        })
+          return this.x(d[0]);
+        }.bind(this))
         .y0(function(d) {
-          return y(d[1]);
-        })
+          return this.y(d[1]);
+        }.bind(this))
         .y1(function(d) {
-          return y(d[2]);
-        });
+          return this.y(d[2]);
+        }.bind(this));
 
-      svg.selectAll("path")
-        .data(data)
-        .enter().append("path")
-        .attr("d", function(d) {
-          return area(d.data)
-        })
-        .attr("fill", function(d) {
-          return colorMap[d.label];
-        })
-        .on("mouseover", function(d) {
-
-          let rect = document.getElementById('div-stream').getBoundingClientRect();
-          let xPosition = d3.event.pageX - rect.x;
-          let yPosition = d3.event.pageY;
-
-          d3.select("#tooltip")
-            .style("left", xPosition + "px")
-            .style("top", yPosition + "px")
-            .select("#value")
-            .text(d.label);
-
-          d3.select("#tooltip").classed("hidden", false);
-        })
-        .on("mouseout", function() {
-          d3.select("#tooltip").classed("hidden", true);
-        });
-
-      function xMax(layer) {
-        let data = layer.data;
-        return d3.max(data, function(d) {
-          return d[0];
-        });
-      }
-
-      function xMin(layer) {
-        let data = layer.data;
-        return d3.min(data, function(d) {
-          return d[0];
-        });
-      }
-
-      function stackMax(layer) {
-        let data = layer.data;
-        return d3.max(data, function(d) {
-          return Math.max(d[1], d[2]);
-        });
-      }
-
-      function stackMin(layer) {
-        let data = layer.data;
-        return d3.min(data, function(d) {
-          return Math.min(d[1], d[2]);
-        });
-      }
+      drawChart.call(this);
     }
+
+    resize(width, height) {
+      d3.select(this.element).select("g").attr("transform", "scale(" + width / this.width + "," + height / this.height + ")");
+      this.width = width;
+      this.height = height;
+    }
+
+    destroy() {
+      d3.select(this.element).select("svg").select("g").selectAll('*').remove();
+    }
+
+    update(data) {
+        this.data = data;
+        console.log(this.data);
+        this.x = d3.scaleLinear()
+          .domain([d3.min(this.data, xMin), d3.max(this.data, xMax)])
+          .range([0, this.width]);
+
+        this.y = d3.scaleLinear()
+          .domain([d3.min(this.data, stackMin), d3.max(this.data, stackMax)])
+          .range([this.height, 0]);
+        this.destroy();
+        drawChart.call(this);
+    }
+
+  }
+
+  function drawChart() {
+
+    this.svg.selectAll("path")
+      .data(this.data)
+      .enter().append("path")
+      .attr("d", function(d) {
+        return this.area(d.data)
+      }.bind(this))
+      .attr("fill", function(d) {
+        return this.colorMap[d.label];
+      }.bind(this))
+      .on("mouseover", function(d) {
+
+        let rect = document.getElementById('div-stream').getBoundingClientRect();
+        let xPosition = d3.event.pageX - rect.x;
+        let yPosition = d3.event.pageY;
+
+        d3.select("#tooltip")
+          .style("left", xPosition + "px")
+          .style("top", yPosition + "px")
+          .select("#value")
+          .text(d.label);
+
+        d3.select("#tooltip").classed("hidden", false);
+      })
+      .on("mouseout", function() {
+        d3.select("#tooltip").classed("hidden", true);
+      });
+
+  }
+
+  function xMax(layer) {
+    let data = layer.data;
+    return d3.max(data, function(d) {
+      return d[0];
+    });
+  }
+
+  function xMin(layer) {
+    let data = layer.data;
+    return d3.min(data, function(d) {
+      return d[0];
+    });
+  }
+
+  function stackMax(layer) {
+    let data = layer.data;
+    return d3.max(data, function(d) {
+      return Math.max(d[1], d[2]);
+    });
+  }
+
+  function stackMin(layer) {
+    let data = layer.data;
+    return d3.min(data, function(d) {
+      return Math.min(d[1], d[2]);
+    });
   }
 
   return StreamGraph;
