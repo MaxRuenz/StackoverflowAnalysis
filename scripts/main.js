af233f5456948f574d36b0b279163b367c21f910
Max Ruenz <max.ruenz@web.de>
2017-12-07 14:41:05 +0100

    Hide carousel arrows when not needed

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,21 @@
 require(['Step0', 'Step1', 'Step2', 'Step3', 'Step4', 'Step5', 'Step6', 'Step7', 'Step8',
-  'chart', 'bootstrap', 'bootstrapSlider'], function(Step0, Step1, Step2, Step3, Step4, Step5, Step6, Step7, Step8, Chart) {
+  'chart', 'bootstrap', 'bootstrapSlider'
+], function(Step0, Step1, Step2, Step3, Step4, Step5, Step6, Step7, Step8, Chart) {
 
   $(function() {
     // Wait until HTML is loaded
 
+    // Carousel is from bootstrap and only working with jQuery
     $('#myCarousel').carousel({
       interval: false,
       wrap: false
     });
 
+    displayArrows();
     Step0.initializePage();
 
     $('#myCarousel').on('slid.bs.carousel', function(event) {
+      displayArrows();
       let page = event.to;
       if (page === 1) {
         Step1.initializePage();
@@ -33,5 +37,18 @@ require(['Step0', 'Step1', 'Step2', 'Step3', 'Step4', 'Step5', 'Step6', 'Step7',
     });
   });
 
+  function displayArrows() {
+    let carousel = $('#myCarousel');
+    if ($('.carousel-inner .carousel-item:first').hasClass('active')) {
+      carousel.children('.carousel-control-prev').hide();
+      carousel.children('.carousel-control-next').show();
+    } else if ($('.carousel-inner .carousel-item:last').hasClass('active')) {
+      carousel.children('.carousel-control-prev').show();
+      carousel.children('.carousel-control-next').hide();
+    } else {
+      carousel.children('.carousel-control-prev').show();
+      carousel.children('.carousel-control-next').show();
+    }
+  }
 
 });
