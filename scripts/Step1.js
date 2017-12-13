090f11d59742c08fcb9c2037b3ab71401904d9b1
Max Ruenz <max.ruenz@web.de>
2017-12-13 12:35:30 +0100

    Create user table dynamically

--- scripts/Step1.js
+++ scripts/Step1.js
@@ -1,6 +1,7 @@
 define(['d3'], function(d3){
 
     let userInformation;
+    const tableElement = '#table-overview'
 
     function initializePage(){
       console.log("Hi");
@@ -9,8 +10,34 @@ define(['d3'], function(d3){
         d3.json('data/users/' + userID +'.json', function(data) {
           console.log(data);
           userInformation = data;
+
+          let tableData = [];
+          tableData.push({"Column": "Reputation", "Value": userInformation.Reputation});
+          tableData.push({"Column": "Questions", "Value": userInformation.data[9]["qcnt"]});
+          tableData.push({"Column": "Question Votes", "Value": userInformation.data[9]["qvotes"]});
+          //tableData.push({"Column": "Avg. Question Votes", "Value": userInformation.data[9]["qcnt"]});
+          tableData.push({"Column": "Answers", "Value": userInformation.data[9]["acnt"]});
+          tableData.push({"Column": "Answer Votes", "Value": userInformation.data[9]["avotes"]});
+          //tableData.push({"Column": "Avg. Answer Votes", "Value": userInformation.data[9]["qcnt"]});
+          tableData.push({"Column": "Votes", "Value": userInformation.data[9]["votes"]});
+
+          createTable(tableData);
         });
       });
+    }
+
+    function createTable(data){
+      d3.select(tableElement).html('');
+
+      let tbody = d3.select(tableElement).append('tbody');
+      let rows = tbody.selectAll('tr')
+                  .data(data)
+                  .enter()
+                  .append('tr')
+      rows.append('th')
+        .text(function (d) {return d.Column})
+      rows.append('td')
+        .text(function (d) {return d.Value})
     }
 
     function getUserInformation(){
